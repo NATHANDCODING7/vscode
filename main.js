8bbd0209219ccd49e9cf82267cb980bbde9cfba0
NATHANDCODING7 <152233195+NATHANDCODING7@users.noreply.github.com>
2023-12-04 20:09:15 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function dark() {
+    document.getElementById("bd1").style.backgroundColor = "black"; document.getElementById("bd1").style.color = "white"; 
+  }
+  
+  function light() { document.getElementById("bd1").style.backgroundColor = "aquamarine";
+    document.getElementById("bd1").style.color = "black"; 
+  }
